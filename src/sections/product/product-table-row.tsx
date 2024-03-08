8177e6c89fbd7fee33563b8232fd23110c349c21
Mustafa Sahin <mustafa@wamo.io>
2024-03-08 10:33:59 +0100

    feat: remove variant create from list menu

--- src/sections/product/product-table-row.tsx
+++ src/sections/product/product-table-row.tsx
@@ -31,7 +31,6 @@ type Props = {
   onSelectRow: VoidFunction;
   onDeleteRow: VoidFunction;
   onEditStock: VoidFunction;
-  onCreateVariants: VoidFunction;
 };
 
 export default function ProductTableRow({
@@ -41,7 +40,6 @@ export default function ProductTableRow({
   onSelectRow,
   onDeleteRow,
   onEditStock,
-  onCreateVariants,
 }: Props) {
   const {
     id,
@@ -141,15 +139,6 @@ export default function ProductTableRow({
         >
           <Iconify icon="eva:cube-fill" />
           {t('stock_update_choices')}
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            onCreateVariants();
-            popover.onClose();
-          }}
-        >
-          <Iconify icon="eva:folder-add-fill" />
-          {t('variant_management')}
         </MenuItem>
         <MenuItem
           onClick={() => {
