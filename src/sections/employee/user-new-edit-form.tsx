3200969a1700e18850fd309172f5a4fd538895a8
Mustafa Sahin <mustafa@wamo.io>
2024-04-05 19:58:01 +0200

    update employee edit form

--- src/sections/employee/user-new-edit-form.tsx
+++ src/sections/employee/user-new-edit-form.tsx
@@ -1,16 +1,15 @@
 import * as Yup from 'yup';
 import moment from 'moment';
-import { useMemo, useState, useEffect } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { useMemo, useEffect } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
+import { Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import LoadingButton from '@mui/lab/LoadingButton';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Divider, MenuItem, Typography } from '@mui/material';
 
 import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hooks';
@@ -20,7 +19,7 @@ import axiosInstance from 'src/utils/axios';
 import { useTranslate } from 'src/locales';
 
 import { useSnackbar } from 'src/components/snackbar';
-import FormProvider, { RHFSelect, RHFSwitch, RHFTextField } from 'src/components/hook-form';
+import FormProvider, { RHFSwitch, RHFTextField } from 'src/components/hook-form';
 
 import { IUserItem } from 'src/types/user';
 
@@ -32,104 +31,13 @@ export default function UserNewEditForm({ currentUser }: Props) {
   const { t, onChangeLang } = useTranslate();
 
   const router = useRouter();
-  const [isBusiness, setIsBusiness] = useState(
-    !['particular', 'admin'].includes(currentUser?.type || 'particular')
-  );
+
   const { enqueueSnackbar } = useSnackbar();
 
   const NewUserSchema = Yup.object().shape({
-    type: Yup.string().required(t('type_required')),
-    first_name: !isBusiness && Yup.string().required(t('name_required')),
-    last_name: !isBusiness && Yup.string().required(t('surname_required')),
     password: currentUser ? null : Yup.string().required(t('password_required')),
     email: Yup.string().required(t('email_required')).email(t('email_must_be_valid')),
-    phone_number:
-      isBusiness &&
-      Yup.string()
-        .required(t('phone_required'))
-        .matches(/^[0-9]+$/, t('phone_number_must_be_numeric')),
-    mobile_number:
-      isBusiness &&
-      Yup.string()
-        .required(t('mobile_required'))
-        .matches(/^[0-9]+$/, t('mobile_number_must_be_numeric')),
-    // gender: Yup.string().required(t('gender_required')),
-    birthdate: Yup.date()
-      .required(t('birthdate_required'))
-      .max(moment().subtract(18, 'years').toDate(), t('birthdate_must_be_before_18_years')),
-    fax: Yup.string().nullable(),
-    facebook: Yup.string().nullable().url(t('facebook_url_invalid')),
-    linkedin: Yup.string().nullable().url(t('linkedin_url_invalid')),
-    twitter: Yup.string().nullable().url(t('twitter_url_invalid')),
-    instagram: Yup.string().nullable().url(t('instagram_url_invalid')),
-    pinterest: Yup.string().nullable().url(t('pinterest_url_invalid')),
-    tiktok: Yup.string().nullable().url(t('tiktok_url_invalid')),
-    notes: Yup.string().nullable(),
-    website: Yup.string().nullable().url(t('website_url_invalid')),
-    is_active: Yup.boolean().required(),
-    is_staff: Yup.boolean().required(),
-    business_name: isBusiness && Yup.string().required(t('business_name_required')),
-    contact_person_name: isBusiness && Yup.string().required(t('contact_person_name_required')),
-    contact_person_phone_number:
-      isBusiness &&
-      Yup.string()
-        .required(t('contact_person_phone_required'))
-        .matches(/^[0-9]+$/, t('contact_person_phone_number_must_be_numeric')),
-    contact_person_email:
-      isBusiness &&
-      Yup.string()
-        .required(t('contact_person_email_required'))
-        .email(t('contact_person_email_invalid')),
-    department: isBusiness && Yup.string().required(t('department_required')),
-    classification: isBusiness && Yup.string().required(t('classification_required')),
-    branch: isBusiness && Yup.string().required(t('branch_required')),
-    iban: isBusiness && Yup.string().required(t('iban_required')),
-    bic: isBusiness && Yup.string().required(t('bic_required')),
-    account_holder_name: isBusiness && Yup.string().required(t('account_holder_name_required')),
-    account_holder_city: isBusiness && Yup.string().required(t('account_holder_city_required')),
-    vat: isBusiness && Yup.string().required(t('vat_required')),
-    kvk: isBusiness && Yup.string().required(t('kvk_required')),
-    payment_method: isBusiness && Yup.string().required(t('payment_method_required')),
-    customer_percentage: isBusiness && Yup.number().required(t('customer_percentage_required')),
-    invoice_discount: isBusiness && Yup.number().required(t('invoice_discount_required')),
-    payment_termin: isBusiness && Yup.string().required(t('payment_termin_required')),
-    credit_limit: isBusiness && Yup.number().required(t('credit_limit_required')),
-    invoice_address: isBusiness && Yup.string().required(t('invoice_address_required')),
-    invoice_language: isBusiness && Yup.string().required(t('invoice_language_required')),
-    discount_group: isBusiness && Yup.string().required(t('discount_group_required')),
-    inform_via: isBusiness && Yup.string().required(t('inform_via_required')),
-    customer_color: isBusiness && Yup.string().required(t('customer_color_required')),
-    relation_type: isBusiness && Yup.string().required(t('relation_type_required')),
-    relation_via: isBusiness && Yup.string().required(t('relation_via_required')),
-    days_closed: isBusiness && Yup.string().required(t('days_closed_required')),
-    days_no_delivery: isBusiness && Yup.string().required(t('days_no_delivery_required')),
-    incasseren: isBusiness && Yup.boolean().required(),
-    is_payment_termin_active: isBusiness && Yup.boolean().required(),
-    is_eligible_to_work_with: isBusiness && Yup.boolean().required(),
-    inform_when_new_products: isBusiness && Yup.boolean().required(),
-    notify: isBusiness && Yup.boolean().required(),
   });
-
-  const USER_TYPES = [
-    { value: 'special', label: t('special') },
-    { value: 'wholesaler', label: t('wholesaler') },
-    { value: 'supermarket', label: t('supermarket') },
-    { value: 'particular', label: t('particular') },
-    { value: 'admin', label: t('admin') },
-  ];
-
-  const CUSTOMER_COLORS = [
-    { value: 'red', label: t('red') },
-    { value: 'yellow', label: t('yellow') },
-    { value: 'green', label: t('green') },
-    { value: 'blue', label: t('blue') },
-    { value: 'brown', label: t('brown') },
-  ];
-  const PAYMENT_METHOD_TYPES = [
-    { value: 'bank', label: t('bank') },
-    { value: 'kas', label: t('kas') },
-    { value: 'pin', label: t('pin') },
-  ];
 
   const defaultValues = useMemo(
     () => ({
@@ -138,21 +46,10 @@ export default function UserNewEditForm({ currentUser }: Props) {
       last_name: currentUser?.last_name || '',
       email: currentUser?.email || '',
       phone_number: currentUser?.phone_number || '',
-      mobile_number: currentUser?.mobile_number || '',
-      // gender: currentUser?.gender || '',
-      type: currentUser?.type || 'admin',
-      birthdate: currentUser?.birthdate || '',
-      fax: currentUser?.fax || null,
-      facebook: currentUser?.facebook || null,
-      linkedin: currentUser?.linkedin || null,
-      twitter: currentUser?.twitter || null,
-      instagram: currentUser?.instagram || null,
-      pinterest: currentUser?.pinterest || null,
-      tiktok: currentUser?.tiktok || null,
+      type: 'admin',
       notes: currentUser?.notes || null,
-      website: currentUser?.website || null,
       is_active: currentUser?.is_active || true,
-      is_staff: currentUser?.is_staff || false,
+      is_staff: currentUser?.is_staff || true,
     }),
     [currentUser]
   );
@@ -277,7 +174,6 @@ export default function UserNewEditForm({ currentUser }: Props) {
                 sm: 'repeat(2, 1fr)',
               }}
             >
-
               <RHFSwitch
                 name="is_active"
                 labelPlacement="start"
