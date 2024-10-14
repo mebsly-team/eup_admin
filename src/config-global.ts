25ec1fdec2c588c4c975ef9de51f81d7cd779abf
M <info@mebsly.com>
2024-10-14 21:13:19 +0200

    update config file

--- src/config-global.ts
+++ src/config-global.ts
@@ -3,8 +3,9 @@ import { paths } from 'src/routes/paths';
 // API
 // ----------------------------------------------------------------------
 
-export const HOST_API = import.meta.env.VITE_HOST_API;
-export const IMAGE_FOLDER_PATH = import.meta.env.VITE_IMAGE_FOLDER_PATH; //https://eup-data.s3.amazonaws.com/eup/uploads/
+export const HOST_API = import.meta.env.VITE_HOST_API || `http://localhost:8000/api`;
+export const IMAGE_FOLDER_PATH =
+  import.meta.env.VITE_IMAGE_FOLDER_PATH || 'https://eup-data.s3.amazonaws.com/eup/uploads/';
 export const ASSETS_API = import.meta.env.VITE_ASSETS_API;
 
 export const MAPBOX_API = import.meta.env.VITE_MAPBOX_API;
