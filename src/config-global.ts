3c616aecc3c770b66aa3655e40ef3431d232759a
M <info@mebsly.com>
2024-11-10 14:01:55 +0100

    update global config

--- src/config-global.ts
+++ src/config-global.ts
@@ -3,7 +3,8 @@ import { paths } from 'src/routes/paths';
 // API
 // ----------------------------------------------------------------------
 
-export const HOST_API = import.meta.env.VITE_HOST_API || `http://localhost:8000/api`;
+export const HOST_API =
+  import.meta.env.VITE_HOST_API || 'https://be.kooptop.com/api' || `http://localhost:8000/api`;
 export const IMAGE_FOLDER_PATH =
   import.meta.env.VITE_IMAGE_FOLDER_PATH || 'https://eup-data.s3.amazonaws.com/eup/uploads/';
 export const ASSETS_API = import.meta.env.VITE_ASSETS_API;
