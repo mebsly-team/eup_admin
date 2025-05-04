e26070dffb07a6802c2fe5f0127c394ede4bd5e7
msahin <info@mebsly.com>
2025-05-04 10:03:18 +0200

    Refactor quantity_per_unit validation in ProductNewEditForm to remove default value transformation, ensuring stricter validation for user input while maintaining required and minimum constraints.

--- src/sections/product/product-new-edit-form.tsx
+++ src/sections/product/product-new-edit-form.tsx
@@ -201,10 +201,7 @@ export default function ProductNewEditForm({ id }: Props) {
         (value) =>
           value === undefined || value === null || /^[0-9]+(\.[0-9]{1,2})?$/.test(value.toString())
       ),
-    quantity_per_unit: Yup.number()
-      .required(t('required'))
-      .min(0.1, t('Moet groter zijn dan 0'))
-      .transform((value) => (value === '' ? 1 : value)),
+    quantity_per_unit: Yup.number().required(t('required')).min(0.1, t('Moet groter zijn dan 0')),
     variant_discount: Yup.number().test(
       'is-decimal',
       t('Max. 2 decimal places'),
