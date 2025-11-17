b91b59680e8040a51fdc50a02760da408e7c4973
msahin <info@mebsly.com>
2025-11-17 19:05:47 +0100

    Refactor IMAGE_FOLDER_PATH in global config to remove environment variable dependency
    - Simplified the IMAGE_FOLDER_PATH assignment in src/config-global.ts to use a static CDN URL, ensuring consistent access to image resources across the application.

--- src/config-global.ts
+++ src/config-global.ts
@@ -5,8 +5,7 @@ import { paths } from 'src/routes/paths';
 
 export const HOST_API =
   import.meta.env.VITE_HOST_API || 'https://be.kooptop.com/api' || `http://localhost:8000/api`;
-export const IMAGE_FOLDER_PATH =
-  import.meta.env.VITE_IMAGE_FOLDER_PATH || 'https://cdn.depotely.com/eup/uploads/';
+export const IMAGE_FOLDER_PATH = 'https://cdn.depotely.com/eup/uploads/';
 export const ASSETS_API = import.meta.env.VITE_ASSETS_API;
 
 export const MAPBOX_API = import.meta.env.VITE_MAPBOX_API;
