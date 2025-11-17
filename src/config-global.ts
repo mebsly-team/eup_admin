fa1305dbde66e9529cf0a43001fface2b0ca500e
msahin <info@mebsly.com>
2025-11-17 11:04:29 +0100

    Update image folder path in Dockerfiles and global config
    - Changed the VITE_IMAGE_FOLDER_PATH in both Dockerfile.dev and Dockerfile.prod to point to the new CDN URL.
    - Updated the IMAGE_FOLDER_PATH in src/config-global.ts to reflect the new CDN URL, ensuring consistency across the application.

--- src/config-global.ts
+++ src/config-global.ts
@@ -6,7 +6,7 @@ import { paths } from 'src/routes/paths';
 export const HOST_API =
   import.meta.env.VITE_HOST_API || 'https://be.kooptop.com/api' || `http://localhost:8000/api`;
 export const IMAGE_FOLDER_PATH =
-  import.meta.env.VITE_IMAGE_FOLDER_PATH || 'https://d3r7qcgexpw3ka.cloudfront.net/';
+  import.meta.env.VITE_IMAGE_FOLDER_PATH || 'https://cdn.depotely.com/eup/uploads/';
 export const ASSETS_API = import.meta.env.VITE_ASSETS_API;
 
 export const MAPBOX_API = import.meta.env.VITE_MAPBOX_API;
